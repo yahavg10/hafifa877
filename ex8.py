e9a58d109cf75b0e95d435e853ff8bbc2e03648e
ערן גבאי <yahavg10@gmail.com>
2024-03-27 10:04:54 +0200

    Revert "done"
    This reverts commit 9ec9db5cf6b17ca1289a9dc7a6ffe59a12fcab41.

--- ex8.py
+++ ex8.py
@@ -1,7 +1,14 @@
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import base64
 
-CONNECTION_STRING = "<private key>"
+CONNECTION_STRING = """DefaultEndpointsProtocol=https;
+EndpointSuffix=core.windows.net;
+AccountName=onboardingpractice;
+AccountKey=2qAbr+yOWtOTrtZ50YLRRDsqp8eLL/zgOXl4pdeiWyanhZ4VkFMqXwd6PMAUBRqy0oTo1QcX2F79+AStaFSy9g==;
+BlobEndpoint=https://onboardingpractice.blob.core.windows.net/;
+FileEndpoint=https://onboardingpractice.file.core.windows.net/;
+QueueEndpoint=https://onboardingpractice.queue.core.windows.net/;
+TableEndpoint=https://onboardingpractice.table.core.windows.net/"""
 
 
 def process_files():
